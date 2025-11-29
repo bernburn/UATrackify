acbe60551024e0ba8022b617b9b62d2f45110053
aiandgreat <ajsgarcia.student@ua.edu.ph>
2025-11-29 13:06:30 +0800

    Updated dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -5,7 +5,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import LoginPage from './pages/auth/LoginPage';
 import RegisterPage from './pages/auth/RegisterPage';
 import DashboardPage from './pages/dashboard/DashboardPage';
-
+import ReviewUserInfoPage from './pages/dashboard/ReviewUserInfoPage';
 const Stack = createNativeStackNavigator();
 
 export default function Dashboard() {
@@ -15,6 +15,7 @@ export default function Dashboard() {
                 <Stack.Screen name="Login" component={LoginPage} options={{headerShown: false}}/>
                 <Stack.Screen name="Register" component={RegisterPage} options={{headerShown: false}}/>
                 <Stack.Screen name="Dashboard" component={DashboardPage} options={{headerShown: false}}/>
+                <Stack.Screen name="ReviewUserInfo" component={ReviewUserInfoPage} options={{headerShown: false}}/>
             </Stack.Navigator>
         </NavigationContainer>
     )
